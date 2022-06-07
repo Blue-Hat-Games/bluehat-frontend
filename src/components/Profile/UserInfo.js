4b1d300bbde90e238020e1cab66575694e97c4e6
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-06-07 23:11:02 +0900

    Feat : Init UserInfo

---
+++ src/components/Profile/UserInfo.js
@@ -0,0 +1,11 @@
+const authHeader = () => {
+    const user = JSON.parse(JSON.parse(localStorage.getItem("user")));
+    if (user && user.accessToken) {
+        return { "Authorization": user.accessToken };
+    } else {
+        return {};
+    }
+};
+
+class UserInfo extends Component {
+}
