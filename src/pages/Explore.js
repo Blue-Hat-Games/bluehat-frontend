cc1069919ddab49f11fa1cac72f6b0536241d42b
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-06-11 22:34:29 +0900

    Feat : Add Release wait alert

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Intro from '../components/Intro';
 import Navbar from '../components/Navbar';
-
+import '../css/Explore.css';
 const Explore = () => {
     return (
         <div>
             <Navbar />
-            <h1>Explore</h1>
-            <Intro />
+            <div className="container repairing">
+            <div class="img">
+            <img src="images/developing.svg" class="reparing-img"></img>
+            </div>
+            <h1 className='title'>Bluehat Games Website is developing</h1>
+            <div className="content">Please wait release 2022.06.24</div>
+            </div>
         </div>
     )
 }
