102adbbee06959fc0e93e68136b8386f243fa231
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-06-13 09:44:30 +0900

    Fix : api server address fix

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use('/', createProxyMiddleware({
-        target: 'http://bluehat.games',
+        target: 'http://api.bluehat.games',
         pathRewrite: {
             '^/': ''
         }
